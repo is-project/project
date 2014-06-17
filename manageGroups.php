6753fa40b5ede3ccca6fc86f13611ac6a83934b9
Marcus <marcus@Samsung-Debian>
2014-06-17 19:41:55 +0200

    manageGroups

---
+++ manageGroups.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once 'inc/init.inc';
+require_once 'inc/validations.inc';
+require_once 'inc/vo_manageProjects.class.inc';
+require_once 'inc/bo_user.class.inc';
+require_once 'inc/bo_project.class.inc';
+
+global $current_user;
+
+?>
