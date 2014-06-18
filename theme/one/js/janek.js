5ec2fd90105903f69ab3996b5b350be13df8478b
jago89 <jago89@users.noreply.github.com>
2014-06-18 10:08:23 +0200

    Delete janek.js

--- theme/one/js/janek.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-	$('ul.projects li').on('click', function() {
-		$('form#add-edit-project-form input#project').val( $(this).find('span:first').attr('project') );
-		$('form#add-edit-project-form input#parent-project').val( $(this).find('span:first').attr('parent-project') );
-		$('form#add-edit-project-form input#project-name').val( $(this).find('span:first').html() );
-		$('form#add-edit-project-form textarea#project-description').val( $(this).find('span:first').attr('title') );
-	});
-});
